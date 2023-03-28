8c089bd746d049b62d5074ef15b196bc13c6feae
Mohit <mohitmajhi99@gmail.com>
2023-03-28 16:39:50 +0530

    added test classs

---
+++ src/test/java/Assignment_5_test.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Assignment_5_test {
+	
+	@Test
+	public void case1()
+	{
+		assertEquals(0, 0);
+	}
+	
+	@Test
+	public void case2()
+	{
+		assertEquals(0, 0);
+	}
+}
